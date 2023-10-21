f7dd846ecb4c73af5862f1ee03fed4c0728a4d26
Pratik Hasmukh Hariya <hariya.p@northeastern.edu>
2023-10-21 14:15:49 -0400

    blogging web application in react

---
+++ client/src/pages/login/Login.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import React, { useContext, useRef } from "react";
+import { Context } from "../../context/Context";
+import "./login.scss";
+
+export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          placeholder="Enter your username..."
+          className="loginInput"
+          ref={userRef}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Enter your password..."
+          className="loginInput"
+          ref={passwordRef}
+        />
+        <button className="loginButton" type="submit" disabled={isFetching}>
+          Login
+        </button>
+      </form>
+      <button className="loginRegisterButton">Register</button>
+    </div>
+  );
+}
